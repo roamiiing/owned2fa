23055f18194b94adf107d1d493fd6068c58b12d6
Vitaliy Lyovkin <unf.hwm@ya.ru>
2023-04-16 23:42:11 +0700

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,34 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'owned2fa',
+        short_name: 'owned2fa',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,gif,svg,woff2}'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
